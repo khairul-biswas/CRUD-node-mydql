797c066af95b2601d4ab654f73853b0b7e246313
Khairul <infokhairul1996@gmail.com>
2023-04-14 09:54:14 +0530

    added ejs

--- app.js
+++ app.js
@@ -1,86 +1,38 @@
 const express = require('express');
 const mysql   = require('mysql');
-
-//Create Connection
-
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'nodemysql'
-});
-
-//Connect
-db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySql Connected...');
-});
-
-
 const app = express();
 
-//Create Database
+// set the view engine to ejs
+app.set('view engine','ejs');
 
-app.get('/createdb', (req,res) => {
-    let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        colsole.log(result);
-        res.send('Database Created');
-    })
-})
+// use res.render to load up an ejs view file 
 
-//Create post data table
-app.get('/createposttable', (req,res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Posts table created...');
-    })
-})
+// index page
+app.get('/', (req,res) => {
+    var mascots = [
+        {name: 'Sammy', organization: "DigitalOcen", bitrh_year: 2012},
+        {name: 'Tux', organization: "Linux", bitrh_year: 1996},
+        {name: 'Moby Dock', organization: "Docker", bitrh_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cuet animal mascot.";
+    res.render('pages/index',{
+        mascots: mascots,
+        tagline: tagline
+    });
+});
 
-//Add a Post
-app.get('/addpost',(req,res) => {
-    let post = {title:'Post Two',body:'This is the test post details'};
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql,post, (err,result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Post 1 added');
-    })
-})
-//Select  Post
-app.get('/getpost',(req,res) => {
-    let sql = 'SELECT * FROM posts';
-    let query = db.query(sql, (err,result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Posts fetched...');
-    })
-})
-//Select single Post
-app.get('/getpost/:id',(req,res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err,result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Posts fetched...');
-    })
-})
-//Update Post
-app.get('/updatepost/:id',(req,res) => {
-    let newTitle = 'UPdate Title';
-    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err,result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Update post title...');
-    })
-})
+// about page
+app.get('/about', (req,res) => {
+    res.render('pages/about');
+});
+
 
 app.listen('3000', () => {
     console.log('server started on port 3000');
 });
+
+
+module.exports = {
+    mysql,
+    app
+}
