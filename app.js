071f5ae2c645ea6b255e854187c9071eb7d1ef50
Khairul Ahamed Biswas <infokhairul1996@gmail.com>
2023-06-20 15:03:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
+const createError = require('http-errors');
 const express = require('express');
 const mysql   = require('mysql');
 const app = express();
+const flash = require('express-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
+//Create Connection
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'nodemysql'
+});
+
+//Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+//app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use(flash());
+app.use(session({ 
+    cookie: { maxAge: 60000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
 // set the view engine to ejs
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // use res.render to load up an ejs view file 
 
@@ -25,10 +57,104 @@ app.get('/', (req,res) => {
 app.get('/about', (req,res) => {
     res.render('pages/about');
 });
+// Posts page
+app.get('/posts', (req,res) => {
+    var sql = 'SELECT * FROM posts';
+    db.query(sql, (err,result) => {
+        if (err) throw err;
+        res.render('pages/posts',{
+            querys: result
+        });
+    })
+    
+});
 
-// about page
+// Posts page
+app.get('/post/(:id)', (req,res) => {
+    let id = req.params.id;
+    var sql = 'SELECT * FROM posts WHERE id = ' + id;
+    db.query(sql, (err,result) => {
+        if (err) throw err;
+        res.render('pages/post',{
+            querys: result
+        });
+    })
+    
+});
+
+// login page
 app.get('/login', (req,res) => {
     res.render('pages/login');
+});
+
+
+// add post page
+app.get('/new-post', (req,res) => {
+    res.render('pages/new-post',{
+        title: '',
+        body: ''
+    });
+});
+app.post('/new-post', (req,res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let errors = false;
+    if(title.length === 0 && body.length === 0){
+        errors = true;
+        req.flash('error', "Please enter title and description");
+        
+    }else{
+        let post = {
+            title: title,
+            body: body
+        };
+        console.log(req.body);
+        
+        var sql = 'INSERT INTO posts SET ?';
+        db.query(sql,post, (err,result) => {
+            if (err) throw err;
+            req.flash('success', 'Post successfully added');
+            res.redirect('/posts');
+        });
+    }
+
+
+
+
+    // if(title === "" || body === ""){
+    //     errors = true;
+    //     req.flash('error', "Please enter title and description");
+    //     res.render('pages/new-post',{
+    //         title: title,
+    //         body: body
+    //     });
+    // }else{
+    //     var form_data = {
+    //         title: title,
+    //         body: body
+    //     }
+    //     var sql = `INSERT INTO posts (title, body) VALUES ('${form_data.title}', '${form_data.body}')`;
+    //     db.query(sql, (err,result) => {
+    //         if (err) {
+    //             req.flash('error', err);
+    //             res.render('pages/new-post',{
+    //                 title: title,
+    //                 body: body
+    //             })   
+    //         }else{
+    //             req.flash('success', 'Post successfully added');
+    //             res.redirect('/posts');
+    //         }
+    //     });
+    // }
+    
+
+
+
+
+    
+
+
 });
 
 app.listen('3000', () => {
